09a2c9b216cd9a1d04e4b056f9163eb8d91a22a6
Paul Evans <pevans1986@gmail.com>
2019-03-10 22:52:59 -0400

    Initial commit

---
+++ Demo/Evans.Demo.Core/Extensions/System/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evans.Demo.Core.Extensions.System
+{
+    public static class StringExtensions
+    {
+        
+    }
+}
