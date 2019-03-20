6f12293f5e026e0b439b7d4bdc54f443e3276347
Paul Evans <pevans1986@gmail.com>
2019-03-19 20:40:41 -0400

    Added Web API project

---
+++ Demo/Evans.Demo.Web.Api/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Evans.Demo.Web.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Demo/Evans.Demo.Web.Api/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Evans.Demo.Web.Api.Startup))]
+
+namespace Evans.Demo.Web.Api
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
