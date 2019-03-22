3f8689d6d1304b08c4086dfbce0034a1dedcef8d
Paul Evans <pevans1986@gmail.com>
2019-03-21 20:56:21 -0400

    Removed generated API help section

--- Demo/Evans.Demo.Web.Api/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace Evans.Demo.Web.Api.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
