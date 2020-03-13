84d7a6b6147c340b544d45dd4cfcfbb32ed2da4a
Paul Evans <pevans1986@gmail.com>
2020-03-13 15:45:51 -0400

    Added MVC common project with custom Handlebars view engine.

---
+++ Demo/Evans.Demo.Core.Web.Mvc/ViewEngines/MvcViewEngineFileSystem.cs
@@ -0,0 +1,28 @@
+﻿using HandlebarsDotNet;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evans.Demo.Core.Web.Mvc.ViewEngines
+{
+	public class MvcViewEngineFileSystem : ViewEngineFileSystem
+	{
+		public override bool FileExists(string filePath)
+		{
+			return File.Exists(filePath);
+		}
+
+		public override string GetFileContent(string filename)
+		{
+			return File.ReadAllText(filename);
+		}
+
+		protected override string CombinePath(string dir, string otherFileName)
+		{
+			return $"{dir}\\{otherFileName}";
+		}
+	}
+}
